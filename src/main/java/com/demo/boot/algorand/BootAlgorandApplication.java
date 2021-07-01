8da8094678a8f5ee9e35460bb982631da88d5aab
umesh <u.rajbhandari@gmail.com>
2021-07-01 17:09:24 +1000

    spring boot starter

---
+++ src/main/java/com/demo/boot/algorand/BootAlgorandApplication.java
@@ -0,0 +1,13 @@
+package com.demo.boot.algorand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootAlgorandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootAlgorandApplication.class, args);
+	}
+
+}
